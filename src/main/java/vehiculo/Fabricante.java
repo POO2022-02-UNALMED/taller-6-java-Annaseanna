156cb8cadb52b2d2439db204dafb4c2970bcf87c
Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
2022-10-04 00:05:41 -0500

    CreoQueElUnico

---
+++ src/main/java/vehiculo/Fabricante.java
@@ -0,0 +1,38 @@
+package vehiculo;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Fabricante {
+    private String nombre;
+    private Pais pais;
+    private static ArrayList<Fabricante> fabricantes= new ArrayList<Fabricante>();
+    protected int cantidadVentas = 0;
+
+    public Fabricante(String nombre, Pais pais) {
+        this.nombre= nombre;
+        this.pais= pais;
+        fabricantes.add(this);
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre= nombre;
+    }
+
+    public Pais getPais(){
+        return pais;
+    }
+    public void setPais(Pais pais){
+        this.pais= pais;
+    }
+
+    public static ArrayList<Fabricante> getFabricantes(){
+        return fabricantes;
+    }
+
+    public static Fabricante fabricaMayorVentas(){
+        return Collections.max(fabricantes, (f1,f2)->f1.cantidadVentas-f2.cantidadVentas);
+    }
+}
